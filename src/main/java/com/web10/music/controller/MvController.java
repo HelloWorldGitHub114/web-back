42f8a7b484eb80d3c038addd475eaf27cf3d293f
ytysuiyuan <467182236@qq.com>
2024-06-01 11:07:32 +0800

    修改Mv相关接口
    Signed-off-by: ytysuiyuan <467182236@qq.com>

--- src/main/java/com/web10/music/controller/MvController.java
+++ src/main/java/com/web10/music/controller/MvController.java
@@ -1,5 +1,7 @@
 package com.web10.music.controller;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.web10.music.commons.result.Result;
 import com.web10.music.entity.Mv;
 import com.web10.music.service.IMvService;
@@ -11,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * <p>
@@ -23,6 +26,7 @@ import javax.annotation.Resource;
 public class MvController {
     @Resource
     private IMvService mvService;
+
     /**
      * 根据mvid查询mv属性
      */
@@ -31,8 +35,72 @@ public class MvController {
     public Result getMvByMvId(@RequestParam Integer mvid){
         Mv mv = mvService.selectMvByMvId(mvid);
         if (mv == null) {
-            return Result.error("400", "该mv不存在");
+            return Result.ok("该mv不存在");
         }
         return Result.ok(mv);
+    }
+
+    /**
+     * 根据mvid推荐相似mv(mv详情页)
+     */
+    @ApiOperation("根据mvid推荐相似mv")
+    @GetMapping("simi")
+    public Result recommendMvByMvId(@RequestParam Integer mvid){
+        Mv mv = mvService.selectMvByMvId(mvid);
+        String artist_name = mv.getArtistName();
+        List<Mv> mvs = mvService.selectMvsByartistName(artist_name, mvid);
+        if (mvs.isEmpty()) {
+            mvs = mvService.randomSelectMvs(mvid);
+            return Result.ok(mvs);
+        }
+        return Result.ok(mvs);
+    }
+
+    /**
+     * 随机推荐mv
+     */
+    @ApiOperation("随机推荐mv")
+    @GetMapping("personalized")
+    public Result randomRecommendMv(@RequestParam Integer limit) {
+        List<Mv> mvs = mvService.randomlySelectMvs(limit);
+        return Result.ok(mvs);
+    }
+
+    /**
+     * 根据mvid查询mv的url
+     */
+    @ApiOperation("根据mvid查询mv的url")
+    @GetMapping("detail")
+    public Result getMvUrlByMvId(@RequestParam Integer mvid){
+        Mv mv = mvService.selectMvByMvId(mvid);
+        if (mv == null) {
+            return Result.ok("该mv不存在");
+        }
+        return Result.ok(mv.getUrl());
+    }
+
+    /**
+     * 获取所有的mv，按照条件进行排序
+     */
+    @ApiOperation("获取所有的mv，按照条件进行排序")
+    @GetMapping("all")
+    public Result getAllMvs(@RequestParam String order, @RequestParam Integer pageNo, @RequestParam Integer pageSize) {
+        // 开启分页
+        PageHelper.startPage(pageNo, pageSize);
+
+        // 按条件查询
+        List<Mv> mvs;
+        if ("NEW".equalsIgnoreCase(order)) {
+            mvs = mvService.getAllMvsOrderBy("publish_time");
+        } else if ("HOT".equalsIgnoreCase(order)) {
+            mvs = mvService.getAllMvsOrderBy("play_count");
+        } else {
+            return Result.ok("Invalid order parameter");
+        }
+
+        // 使用PageInfo封装结果
+        PageInfo<Mv> pageInfo = new PageInfo<>(mvs);
+
+        return Result.ok(pageInfo);
     }
 }
